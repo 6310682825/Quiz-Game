5212ebc7b20dd39df11d5f7d358cb44e2147e1c9
Supachai Matsuwannakit <6310530016@cn330>
2023-03-25 16:57:26 +0700

    added question

--- app/src/main/java/com/example/quizgame/data/WordsData.kt
+++ app/src/main/java/com/example/quizgame/data/WordsData.kt
@@ -7,23 +7,53 @@ const val SCORE_INCREASE = 20
 val allQuestions: Set<Questions> =
     setOf(
         Questions(
-            "Test1",
-            listOf("A1", "A2,", "A3", "A4"),
-            "A1"
+            "What is the square root of 16?",
+            listOf("2", "4", "6", "8"),
+            "1"
         ),
         Questions(
-            "Test2",
-            listOf("A1", "A2,", "A3", "A4"),
-            "A4"
+            "What is the capital of France?",
+            listOf("Madrid", "Paris", "London", "Berlin"),
+            "1"
         ),
         Questions(
-            "Test3",
-            listOf("A1", "A2,", "A3", "A4"),
-            "A4"
+            "What is the tallest mountain in the world?",
+            listOf("Mount Everest", "K2", "Kilimanjaro", "Denali"),
+            "0"
         ),
         Questions(
-            "Test4",
-            listOf("A2", "A2,", "A3", "A4"),
-            "A1"
+            "What is the largest planet in our solar system?",
+            listOf("Jupiter", "Saturn", "Neptune", "Mars"),
+            "0"
         ),
+        Questions(
+            "What is the smallest country in the world?",
+            listOf("Vatican City", "Monaco", "Nauru", "Liechtenstein"),
+            "0"
+        ),
+        Questions(
+            "What is the chemical symbol for gold?",
+            listOf("Go", "Ag", "Au", "Cu"),
+            "2"
+        ),
+        Questions(
+            "What is the boiling point of water?",
+            listOf("100°C", "0°C", "50°C", "200°C"),
+            "0"
+        ),
+        Questions(
+            "What is the largest organ in the human body?",
+            listOf("Liver", "Lungs", "Heart", "Skin"),
+            "3"
+        ),
+        Questions(
+            "What is the currency of Japan?",
+            listOf("Yuan", "Yen", "Dollar", "Won"),
+            "1"
+        ),
+        Questions(
+            "What is 2+2?",
+            listOf("3", "4", "5", "6"),
+            "1"
+        )
     )
