b16df50334fb6adb6ba4ef586e6c92a7dc130f10
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-04-03 18:57:41 +0700

    Update dependency (FINALLY)

--- app/Pumpuy/Quiz.kt
+++
@@ -1,5 +0,0 @@
-data class Question(
-    val text: String,
-    val options: List<String>,
-    val answer: Int
-)
--- app/src/main/java/com/example/quizgame/data/WordsData.kt
+++ app/src/main/java/com/example/quizgame/data/WordsData.kt
@@ -1,6 +1,6 @@
 package com.example.quizgame.data
 
-const val MAX_NO_OF_WORDS = 10
+const val MAX_NO_OF_QUESTIONS = 10
 const val SCORE_INCREASE = 20
 
 // Set with all the words for the Game
@@ -9,51 +9,51 @@ val allQuestions: Set<Questions> =
         Questions(
             "What is the square root of 16?",
             listOf("2", "4", "6", "8"),
-            "1"
+            "4"
         ),
         Questions(
             "What is the capital of France?",
             listOf("Madrid", "Paris", "London", "Berlin"),
-            "1"
+            "Paris"
         ),
         Questions(
             "What is the tallest mountain in the world?",
             listOf("Mount Everest", "K2", "Kilimanjaro", "Denali"),
-            "0"
+            "Mount Everest"
         ),
         Questions(
             "What is the largest planet in our solar system?",
             listOf("Jupiter", "Saturn", "Neptune", "Mars"),
-            "0"
+            "Jupiter"
         ),
         Questions(
             "What is the smallest country in the world?",
             listOf("Vatican City", "Monaco", "Nauru", "Liechtenstein"),
-            "0"
+            "Vatican City"
         ),
         Questions(
             "What is the chemical symbol for gold?",
             listOf("Go", "Ag", "Au", "Cu"),
-            "2"
+            "Au"
         ),
         Questions(
             "What is the boiling point of water?",
             listOf("100°C", "0°C", "50°C", "200°C"),
-            "0"
+            "100°C"
         ),
         Questions(
             "What is the largest organ in the human body?",
             listOf("Liver", "Lungs", "Heart", "Skin"),
-            "3"
+            "Skin"
         ),
         Questions(
             "What is the currency of Japan?",
             listOf("Yuan", "Yen", "Dollar", "Won"),
-            "1"
+            "Yen"
         ),
         Questions(
             "What is 2+2?",
             listOf("3", "4", "5", "6"),
-            "1"
+            "4"
         )
     )
