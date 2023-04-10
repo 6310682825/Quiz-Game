2f6bf340f5e21e4753ae061a29bcd50164923ddf
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-04-10 12:28:23 +0700

    Updatable score

--- app/src/main/java/com/example/quizgame/ui/QuizViewModel.kt
+++ app/src/main/java/com/example/quizgame/ui/QuizViewModel.kt
@@ -1,27 +1,31 @@
 package com.example.quizgame.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.quizgame.data.Questions
 import com.example.quizgame.data.allQuestions
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class QuizViewModel : ViewModel() {
 
-    init {
-        resetGame()
-    }
     private val _uiState = MutableStateFlow(QuizUiState())
     private lateinit var currentQuestion: Questions
     private var questionsAsked: MutableSet<Questions> = mutableSetOf()
     val uiState: StateFlow<QuizUiState> = _uiState.asStateFlow()
 
+    init {
+        resetGame()
+    }
     private fun pickRandomQuestion(): Questions {
         currentQuestion = allQuestions.random()
         if (questionsAsked.contains(currentQuestion)) {
+            Log.i("test","questions asked")
             return pickRandomQuestion()
-        } else {
+        }
+        else {
             questionsAsked.add(currentQuestion)
             return shuffleOption(currentQuestion)
         }
@@ -32,7 +36,41 @@ class QuizViewModel : ViewModel() {
         return question
     }
 
+    private fun updateQuizState(updatedScore: Int) {
+        if (questionsAsked.size > 10) {
+            _uiState.update { currentQuestion ->
+                currentQuestion.copy(
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        }
+        else {
+            _uiState.update { currentQuestion ->
+                currentQuestion.copy(
+                    currentQuestion = pickRandomQuestion(),
+                    score = updatedScore,
+                    questionCount = currentQuestion.questionCount.inc()
+                )
+            }
+        }
+    }
+
+    fun skipQuestion() {
+        updateQuizState(_uiState.value.score)
+    }
+    fun checkAnswer(guess: String, answer:String) {
+        val updatedScore: Int
+        if (guess === answer) {
+            updatedScore = _uiState.value.score.plus(10)
+        }
+        else {
+            updatedScore = _uiState.value.score.minus(5)
+        }
+        updateQuizState(updatedScore)
+    }
     fun resetGame() {
+        Log.i("text", "It's working!")
         questionsAsked.clear()
         _uiState.value = QuizUiState(currentQuestion = pickRandomQuestion())
     }
